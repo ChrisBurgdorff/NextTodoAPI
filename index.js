8464052a945b6dd5793a3ecdf9bd68d0a796e11d
Christopher Burgdorff <cburgdorff@pringle-quinn.com>
2022-04-02 18:40:03 -0400

    Added some routes, works with frontend

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
+var cors = require('cors');
+var appConfig = require('./config');
 
 const db = require('./models');
-
-const { Todo } = require('./models');
+const { Todo, User } = require('./models');
 
 /*
-Installed packages: express, mysql2, sequelize, sequelize-cli, path, fs
+Installed packages: express, mysql2, sequelize, sequelize-cli, path, fs, cors
 
 Using the Sequelize CLI:
 sequelize init
@@ -16,12 +17,17 @@ then set up models
 Tables will be created and synced automatically
 */
 
+//Middleware
 app.use(express.json());
+app.use(cors(appConfig.CORS_OPTIONS));
+app.use(express.urlencoded({ extended: true }));
 
+//Test route
 app.get('/', function(req, res) {
   res.send('hhhii');
 });
 
+//Connect to DB and start server
 db.sequelize.sync().then((req) =>{
   app.listen(process.env.port || 3001, function() {
     console.log("Listening on " + (process.env.port || 3001));
@@ -57,7 +63,7 @@ app.delete('/api/todo/:id', (req, res) => {
       id: req.params.id
     }
   }).then(todo => {
-    res.send({message: "Success"});
+    res.json({message: "Success"});
   }).catch(err => {
     res.send(err);
   });
@@ -71,6 +77,21 @@ app.get('/api/todo/:id', (req, res) => {
   }).then(todo => {
     res.send(todo);
   }).catch(err =>{
+    res.send(err);
+  });
+});
+
+app.put('/api/todo/:id', (req, res) => {
+  const id = req.params.id;
+  Todo.update(req.body, {
+    where: { id: id }
+  }).then(todo => {
+    console.log("RESPONSE FROM PUTT");
+    console.log(todo);
+    res.send(todo);
+  }).catch(err => {
+    console.log("RESPONSE FROM PUT ERROR");
+    console.log(err);
     res.send(err);
   });
 });
